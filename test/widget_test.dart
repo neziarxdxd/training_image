2f9d424a343a3140b8cec5fc26efa8ef47cae661
zenraiching <rickdavidsangalang@gmail.com>
2021-07-05 21:06:55 +0800

    add

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:training_image/Home.dart';
 
 import 'package:training_image/main.dart';
 
